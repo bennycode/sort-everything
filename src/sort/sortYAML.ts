aafbee5632e673b48c200d06f90ae057f171b058
Florian Imdahl <ffflorian@users.noreply.github.com>
2022-10-19 21:24:21 +0200

    fix: Keep comments when sorting yaml (#2)

--- src/sort/sortYAML.ts
+++ src/sort/sortYAML.ts
@@ -1,10 +1,22 @@
-import {dump, load} from 'js-yaml';
+import {ParsedNode, parseDocument, YAMLMap, YAMLSeq} from 'yaml';
 import type {SortFunction} from './SortFunction';
+
+function sortDeep(node: ParsedNode | null): void {
+  if (node instanceof YAMLMap) {
+    node.items.sort((itemA, itemB) => (itemA.key < itemB.key ? -1 : itemA.key > itemB.key ? 1 : 0));
+    node.items.forEach(item => sortDeep(item.value));
+  } else if (node instanceof YAMLSeq) {
+    node.items.forEach(item => sortDeep(item));
+  }
+}
 
 export const sortYAML: SortFunction = function (text: string) {
   try {
+    const document = parseDocument(text);
+    sortDeep(document.contents);
+
     return {
-      payload: dump(load(text), {sortKeys: true}),
+      payload: document.toString(),
       type: 'success',
     };
   } catch (error) {
