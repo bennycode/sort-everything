e08ea58c1bcde206ba7625f813341c254fe71371
Benny Neugebauer <mail@bennycode.com>
2025-05-21 12:32:24 +0200

    refactor: Simplify type checking

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,23 @@
-//@ts-check
-
+// @ts-check
 const path = require('path');
 
-//@ts-check
-/** @typedef {import('webpack').Configuration} WebpackConfig **/
-
-/** @type WebpackConfig */
-const extensionConfig = {
-  target: 'node',
-  mode: 'none',
-
-  entry: './src/extension.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'extension.js',
-    libraryTarget: 'commonjs2',
-  },
-  externals: {
-    vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
-    // modules added here also need to be added in the .vscodeignore file
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
+/** @type {import('webpack').Configuration} */
+const config = {
+  entry: "./src/index.js",
+  mode: "development",
   module: {
     rules: [
       {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader',
-          },
-        ],
-      },
-    ],
+        exclude: /(node_modules)/,
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader"
+      }
+    ]
   },
-  devtool: 'nosources-source-map',
-  infrastructureLogging: {
-    level: 'log', // enables logging required for problem matchers
+  output: {
+    path: path.resolve("./dist")
   },
+  plugins: []
 };
-module.exports = [extensionConfig];
+
+export default config;
