cd9418b9af25b2a2fa82fce711035618d06a6d45
Benny Neugebauer <mail@bennycode.com>
2025-05-22 19:36:30 +0200

    fix: Revert Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,39 @@
 const path = require('path');
 
 /** @type {import('webpack').Configuration} */
-const config = {
-  entry: "./src/index.js",
-  mode: "development",
+const extensionConfig = {
+  target: 'node',
+  mode: 'none',
+
+  entry: './src/extension.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+  },
+  externals: {
+    vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    // modules added here also need to be added in the .vscodeignore file
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
   module: {
     rules: [
       {
-        exclude: /(node_modules)/,
-        test: /\.(js|jsx)$/i,
-        loader: "babel-loader"
-      }
-    ]
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+    ],
   },
-  output: {
-    path: path.resolve("./dist")
+  devtool: 'nosources-source-map',
+  infrastructureLogging: {
+    level: 'log', // enables logging required for problem matchers
   },
-  plugins: []
 };
-
-export default config;
+module.exports = [extensionConfig];
