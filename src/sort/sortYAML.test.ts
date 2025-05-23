e3cd4131523066e8461c46605258e35b8c7539f3
Benny Neugebauer <mail@bennycode.com>
2025-05-23 13:15:38 +0200

    preserve whitespace

--- src/sort/sortYAML.test.ts
+++ src/sort/sortYAML.test.ts
@@ -1,15 +1,18 @@
 import {sortYAML} from './sortYAML';
 import assert from 'node:assert';
+import fs from 'node:fs';
+import path from 'node:path';
 
 describe('sortYAML', () => {
   it('preserves comments when sorting YAML input', () => {
-    const input = `
-variables:
-  C_VARIABLE: 'true' # Comment C
-  A_VARIABLE: 'true' # Comment A
-  B_VARIABLE: 'true' # Comment B
-  `;
+    const input = fs.readFileSync(path.join(__dirname, '../test/fixtures/sort-comments.yml'), 'utf-8');
+    const actual = sortYAML(input);
+    assert(actual.type === 'success');
+    expect(actual.payload).toMatchSnapshot();
+  });
 
+  it('preserves leading whitespace', () => {
+    const input = fs.readFileSync(path.join(__dirname, '../test/fixtures/leading-whitespace.yml'), 'utf-8');
     const actual = sortYAML(input);
     assert(actual.type === 'success');
     expect(actual.payload).toMatchSnapshot();
